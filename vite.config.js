4d74eadd3ba716cb5c2a681241e5414e5439ca90
Andy Blum <andy.blum.01@gmail.com>
2023-12-07 23:57:46 -0500

    Add local livereloading

---
+++ vite.config.js
@@ -0,0 +1,12 @@
+/** @type {import('vite').UserConfig} */
+const config = {
+    // base: "./",
+    root: "./demos",
+    server: {
+        hmr: {
+            clientPort: 5111
+        }
+    }
+};
+
+export default config;
